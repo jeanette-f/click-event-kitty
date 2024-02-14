d66146af3d0bc6ce377adc6c97fe22603cf01217
j_ferlan <jeanette.e.carroll@gmail.com>
2024-02-13 16:17:53 -0800

    Add disappear class to button

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ var cat = document.querySelector(".cat");
 
 
 button.addEventListener("click", function() {
-    button.innerText = "It's working.";
-    cat.classList.add ("show");
-}
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!!";
+        button.classList.remove("disappear");
+    }
+} 
 );
